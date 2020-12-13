447ba14de98b147e9ceeae3bbff2e13b4a173239
ulf <ulf@LAPTOP-RNVUDMQV.alltele.net>
2020-12-13 10:44:37 +0100

    alla rader i listor läggs till varukorg

--- src/java/se/saljex/hemsida/ListorServlet.java
+++ src/java/se/saljex/hemsida/ListorServlet.java
@@ -38,9 +38,8 @@ public class ListorServlet extends HttpServlet {
         boolean contentOnly = Const.getInitData(request).isContentOnlyCall();
         try (PrintWriter out = response.getWriter()) {
             if (!contentOnly) request.getRequestDispatcher("/WEB-INF/site-header.jsp").include(request, response);
-         SessionData sd = Const.getSessionData(request);
+            SessionData sd = Const.getSessionData(request);
             Connection con = Const.getConnection(request);
-            out.print("listor");
             if (sd.isUserInloggad()) {
                 try {
                     PreparedStatement ps = con.prepareStatement("select case when coalesce(o1.marke,'')<>'' then o1.marke else o1.offertnr::varchar end as marke, o2.artnr, ceiling(o2.best / case when coalesce(a.minsaljpack,0) = 0 then 1 else a.minsaljpack end) as best  from offert1 o1 join offert2 o2 on o1.offertnr=o2.offertnr and o2.artnr not like 'Z%' join artikel a on a.nummer=o2.artnr\n" +
@@ -49,20 +48,30 @@ public class ListorServlet extends HttpServlet {
                     ps.setString(1, sd.getInloggadKundnr());
                     ResultSet rs = ps.executeQuery();
                     int rowCn=0;
+                    int listCn=0;
                     String tempMarke = null;
                     out.print("<div class=\"lst\">");
                     while (rs.next()) {
                         rowCn++;
                         if (tempMarke== null || !tempMarke.equals(rs.getString("marke"))) {
                             if (tempMarke != null) out.print("</div>");
+                            listCn++;
                             tempMarke = rs.getString("marke");
                             out.print("<div class=\"lst-grupp\">");
                                 out.print("<div class=\"lst-grupp-header\">");
                                 out.print(SXUtil.toHtml(rs.getString("marke")));
+                                out.print("<button onclick=\"" +
+                                        " var i; var x = document.getElementsByName('btn_list" + listCn + "'); " +
+                                        " for (i = 0; i < x.length; i++) {" +
+                                            "if (typeof x[i].onclick == 'function') { " +
+                                            "    x[i].onclick.apply(x[i]); " + 
+                                            " } " +
+                                        " } " +
+                                    " \"> Alla </button>");
                                 out.print("</div>");
                         }
                         VarukorgRow vk = SQLHandler.getArtikelProdukt(con, rs.getString("artnr"), null, sd.getInloggadKundnr(), sd.getLagerNr());
-                        
+                        vk.getArt().addLagerSaldoRow(SQLHandler.getLagerSaldo(con, vk.getArt().getArtnr(), sd.getLagerNr()));
                         out.print("<div id=\"variant-" + rowCn + "\" aid=\"" + vk.getArt().getArtnr() + "\" class=\"lst-row "+ (rowCn%2 == 0 ? "" : "lst-row-odd")  +"\">");
                             out.print("<div class=\"vk-img vkf-img lst-row-img\">");
                                 out.print("<img src=\"");
@@ -83,10 +92,10 @@ public class ListorServlet extends HttpServlet {
                             out.print("<div class=\"lst-row-antal\">");
                                 out.print("<input id=\"antalinput-" + rowCn + "\" size=\"4\" maxlength=\"4\" value=\"" + rs.getInt("best") + "\">");
                                 out.print(" " + (vk.getArt().getAntalSaljpack().equals(1.0) ? Const.getFormatEnhet(vk.getArt().getEnhet()) : Const.getAnpassade2Decimaler(vk.getArt().getAntalSaljpack()) + vk.getArt().getFormatEnhet()));
+                                out.print("<br>Saldo: " + vk.getArt().getLagerSaldoString(sd.getLagerNr(), true));
                             out.print("</div>");
-                            
                             out.print("<div class=\"lst-row-kop\">");
-                                out.print("<div class=\"t-variant-kop a-btn lst-kop-btn\" onclick=\"vk_add(" + vk.getP().getKlasid() + ", 'variant-" + rowCn + "','antalinput-" + rowCn + "');\">" + StartupData.getLanguage().Kop() +"</div>");
+                                out.print("<div name=\"btn_list" + listCn +"\" class=\"t-variant-kop a-btn lst-kop-btn\" onclick=\"vk_add(" + vk.getP().getKlasid() + ", 'variant-" + rowCn + "','antalinput-" + rowCn + "'); this.innerHTML='&check;';\">" + StartupData.getLanguage().Kop() +"</div>");
                             out.print("</div>");
                         out.print("</div>");
                         
